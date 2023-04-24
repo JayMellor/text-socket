b25e85a9819f822e8758fcbf88721d7230aac200
Jay Mellor <jaymellor@Jays-MacBook-Pro.local>
2023-04-24 20:03:05 +0100

    Added response handling and frontend state management

--- src/main.lisp
+++ src/main.lisp
@@ -58,9 +58,9 @@
 		 "Badly formed request")
 		((equalp (getf request :command) "name")
 		 (setf (getf state :name) (getf request :content))         
-		 (format nil "Welcome ~a!" (getf state :name)))
+		 (format nil ":name ~a" (getf state :name)))
 		(t
-		 (format nil "Command ~a not found" (getf request :command)))))
+		 (format nil ":error command not found ~a" (getf request :command)))))
 
 (defun update-state (request state)
   (cond ((null (getf request :name))
@@ -71,18 +71,54 @@
 	(format (usocket:socket-stream connection) text)                
 	(force-output (usocket:socket-stream connection))))
 
-(defun create-client (port)  
-  (loop
-	(usocket:with-client-socket (socket stream +localhost+ port :element-type 'character)
-	  (format t "Welcome to Text Socket!~% Please enter your name:~%")
-	  (let* ((command (read-line))
-			 (request (create-request command)))        
-		(send-over-connection socket (format nil "~a~%" request)))
-	  (usocket:wait-for-input socket)
-	  (format t "~a~%" (read-line stream)))))
+(defun create-client (port)
+  (let ((state nil)
+		(continue? t ))
+	(loop
+	  while (not (null continue?))
+	  do (usocket:with-client-socket (socket stream +localhost+ port :element-type 'character)
+           (format t "~a~%" (message-from-state state))
+		   (format t "~a:> " (if (and (listp state)
+									   (getf state :name))
+								(getf state :name)
+								""))
+		   (let* ((command (read-line)))
+			 (if (equalp command "exit")
+				 (setf continue? nil)
+				 (progn 
+				   (let ((request (create-request state command)))
+					 (send-over-connection socket (format nil "~a~%" request)))
+				   (usocket:wait-for-input socket)
+				   (let* ((response (read-line stream))
+						  (parsed (parse-response response)))                     
+					 (when (not (null parsed))
+					   (setf (getf state
+								   (first parsed))
+							 (second parsed)))))))))))
 
-(defun create-request (command)
-  (format nil ":command name :content ~a" command))
+(defun message-from-state (state)
+  (cond ((not (listp state))
+		 "Whoops there's something off here. Please close and restart this session!")
+		((or (null state)
+			 (null (getf state :name)))         
+		 (format nil "Welcome to Text Socket!~%Please enter your name:"))        
+		(t (format nil "Well ~a, what now?" (getf state :name)))))
+
+(defun create-request (state command)  
+  (if (null state)
+	  (format nil ":command name :content ~a" command)
+	  ;; todo
+	  nil))
+
+(defun parse-response (response)  
+  (cond
+	((or (null response)
+		 (not (stringp response)))
+	 nil)
+	((let ((search-name (search ":name" response)))
+	   (and (not (null search-name))
+			(zerop search-name)))     
+	 (list :name (string-trim " " (subseq response (length ":name")))))))
 
 ;; ~% very important for read-line
 ;; are defconstants appropriate here?
